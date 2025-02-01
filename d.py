a4e78dda9d163edac9ea79663fb5df0f74a147ee
dlwnstn359 <dlwnstn519@gmail.com>
2025-02-02 00:49:01 +0900

    Update d.py

--- d.py
+++ d.py
@@ -1,2 +1,7 @@
 import streamlit as st
-st.title("                                       독도 퀴즈")
+st.set_page_config(
+    page_title="독도 퀴즈",
+    page_icon="🇰🇷",
+    layout="centered"
+)
+st.title("독도 퀴즈")
