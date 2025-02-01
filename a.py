d5ecb39538741432aeb9b0eac36aae2b923e1d2d
dlwnstn359 <dlwnstn519@gmail.com>
2025-02-02 00:54:59 +0900

    Update a.py

--- a.py
+++ a.py
@@ -38,7 +38,7 @@ st.markdown(
 st.markdown('<h1 class="title">🇰🇷 독도, 대한민국의 자랑 🇰🇷</h1>', unsafe_allow_html=True)
 
 # 소개글
-st.markdown('<p class="description">:red[독] :blue[도] 지킴이 홈페이지에 오신것을 환영합니다! 독도의 자연,역사,위치 등 독도에 관한 정보를 알려드립니다. 표어와 퀴즈를 구경할 수 있습니다.</p>', unsafe_allow_html=True)
+st.markdown(''' :red[독] :blue[도] 지킴이 홈페이지에 오신것을 환영합니다! 독도의 자연,역사,위치 등 독도에 관한 정보를 알려드립니다. 표어와 퀴즈를 구경할 수 있습니다.''')
 
 
 # 사용자 채팅 입력
