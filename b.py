4df13b6e8271c80b50206343a60d9aeca9481e1e
dlwnstn359 <dlwnstn519@gmail.com>
2025-02-02 00:47:18 +0900

    Update b.py

--- b.py
+++ b.py
@@ -1,4 +1,9 @@
 import streamlit as st
+st.set_page_config(
+    page_title="독도 역사",
+    page_icon="🇰🇷",
+    layout="centered"
+)
 st.title("독도 역사")
 if st.button('더 보기'):
   st.image("common.jpg", caption="독도 사진", use_column_width=True)
