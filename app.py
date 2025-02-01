3c3b8428d0fa273edbfe50fdceb978d7b149eeca
dlwnstn359 <dlwnstn519@gmail.com>
2025-02-02 00:21:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from PIL import Image
 pages = {
     "Your account": [
         st.Page("a.py", title="메인"),
-        st.Page("b.py", title="독도 정보"),
-        st.Page("c.py", title="독도 표어"),
+        st.Page("b.py", title="독도 역사"),
+        st.Page("c.py", title="독도 자연"),
         st.Page("d.py", title="독도 퀴즈"),
     ]
     }
