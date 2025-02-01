e7b3786af864ed7b5ba0e69231782541635c9b0d
dlwnstn359 <dlwnstn519@gmail.com>
2025-02-02 01:05:45 +0900

    Update b.py

--- b.py
+++ b.py
@@ -4,4 +4,30 @@ st.set_page_config(
     page_icon="🇰🇷",
     layout="centered"
 )
+
+# 배경 스타일 설정
+st.markdown(
+    """
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Nanum+Myeongjo:wght@700&display=swap');
+    .stApp {
+        background-color: #f0f8ff;
+        background-image: linear-gradient(45deg, #f0f8ff 25%, #d7e3fc 75%);
+    }
+    .title {
+        font-family: 'Nanum Myeongjo', serif;
+        font-size: 3rem;
+        text-align: center;
+        color: #2c3e50;
+    }
+    .description {
+        font-size: 1.2rem;
+        text-align: center;
+        color: #34495E;
+        margin-bottom: 20px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 st.title("독도 역사")
