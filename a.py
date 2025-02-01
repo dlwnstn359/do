1bb9d0a9a169832c9df817aa43da66ad04aa8f5c
dlwnstn359 <dlwnstn519@gmail.com>
2025-02-02 01:43:29 +0900

    Update a.py

--- a.py
+++ a.py
@@ -35,20 +35,20 @@ st.markdown(
 )
 st.snow()
 # 제목
-st.markdown('<h1 class="title"> 독도 지킴이 </h1>', unsafe_allow_html=True)
+st.title(' :gray[독도 지킴이] ')
 
 # 소개글
-st.markdown(''' :red[독도] 지킴이:ocean: 홈페이지에 오신것을 환영합니다! 독도의 자연,역사,위치 등 독도에 관한 정보를 알려드립니다. 표어와 퀴즈를 구경할 수 있습니다.''')
+st.markdown(''' :red[독도] :gray[지킴이]:ocean: :gray[홈페이지에 오신것을 환영합니다! 독도의 자연,역사,위치 등 독도에 관한 정보를 알려드립니다. 표어와 퀴즈를 구경할 수 있습니다.] ''')
 
 st.image('독도 메인.jpg')
 
-st.header("표어")
+st.header(":gray[표어]")
 
 st.markdown(
     """
     ---
     
-    🌐 **독도 지킴이** | 만든이: [이준수] | 모든 권리 보유.
+    🌐 :gray[**독도 지킴이** | 만든이: [이준수] | 모든 권리 보유.]
 
     ---
     
@@ -56,5 +56,5 @@ st.markdown(
 )
 
 # 사용자 채팅 입력
-st.text_input("💬 독도에 대해 하고 싶은 말을 남겨보세요!", placeholder="여기에 입력하세요...")
+st.text_input("💬 :gray[독도에 대해 하고 싶은 말을 남겨보세요!", placeholder="여기에 입력하세요...]")
 
