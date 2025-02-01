fc2fa9839498514bdafc15d65e067d595d131f0e
dlwnstn359 <dlwnstn519@gmail.com>
2025-02-02 00:15:40 +0900

    Update b.py

--- b.py
+++ b.py
@@ -1,5 +1,4 @@
 import streamlit as st
 st.title("독도 역사")
-st.subheader("더 알아보기")
-  dokdo_image = Image.open(r"C:\Users\VIEW\Downloads\common.jpg")
-  st.image(dokdo_image, caption="대한민국의 자랑, 독도", use_column_width=True)
+if st.button('더 보기'):
+  st.image("C:\Users\VIEW\Downloads\common.jpg")
