0465fe8f2fadf2519ea3a925030673f2cba82b56
dlwnstn359 <dlwnstn519@gmail.com>
2025-02-02 02:29:39 +0900

    Update d.py

--- d.py
+++ d.py
@@ -6,31 +6,7 @@ st.set_page_config(
 )
 
 
-# 배경 스타일 설정
-st.markdown(
-    """
-    <style>
-    @import url('https://fonts.googleapis.com/css2?family=Nanum+Myeongjo:wght@700&display=swap');
-    .stApp {
-        background-color: #f0f8ff;
-        background-image: linear-gradient(45deg, #f0f8ff 25%, #d7e3fc 75%);
-    }
-    .title {
-        font-family: 'Nanum Myeongjo', serif;
-        font-size: 3rem;
-        text-align: center;
-        color: #2c3e50;
-    }
-    .description {
-        font-size: 1.2rem;
-        text-align: center;
-        color: #34495E;
-        margin-bottom: 20px;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+
 st.header(":gray[표어]")
 
 st.snow()
