283e78530e16baba10783aee9b9a9432263053a9
dlwnstn359 <dlwnstn519@gmail.com>
2025-02-02 00:20:06 +0900

    Update b.py

--- b.py
+++ b.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.title("독도 역사")
 if st.button('더 보기'):
-  st.image("common.jpg", caption="독도 사진", use_column_width=True
+  st.image("common.jpg", caption="독도 사진", use_column_width=True)
