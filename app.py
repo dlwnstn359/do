aeccfd6adc4b1bdb943f523636f5b12d931040a8
dlwnstn359 <dlwnstn519@gmail.com>
2025-02-03 00:02:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,18 +4,18 @@ from PIL import Image
 
 
 pages = {
+    st.image("독도 메인.jpg"),
     "Your account": [
         st.Page("a.py", title="메인"),
         st.Page("b.py", title="독도 역사"),
         st.Page("c.py", title="독도 자연"),
         st.Page("d.py", title="독도 퀴즈"),
         st.Page("e.py", title="독도 표어"),
+        st.image("독도 메인.jpg")
     ]
     }
 
-# 사이드바에 로고 이미지 추가
-logo = Image.open("독도 메인.jpg")  
-st.sidebar.image(logo, use_column_width=True)
+
 
 pg = st.navigation(pages)
 pg.run()
